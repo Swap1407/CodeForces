5b5b5f99884cf641577766f0cb8a396e65201923
Swapnil S Sawat <swapnilssawat@gmail.com>
2022-04-04 14:22:48 +0530

    Added solutions

---
+++ 158A-Next Roun.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n,k;
+    cin>>n>>k;
+    int count=0;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    for(int i=0;i<n;i++){
+        if(arr[k-1]<=arr[i] && arr[k-2] != 0){
+            count++;
+        }
+    }
+    cout<<count<<endl;
+    return 0;
+}
