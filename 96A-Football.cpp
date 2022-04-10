e920535ece4ebea7fa18c2ef66d1a70a8534cdc1
Swapnil S Sawat <swapnilssawat@gmail.com>
2022-04-10 19:15:41 +0530

    implementation, strings

---
+++ 96A-Football.cpp
@@ -0,0 +1,53 @@
+/*
+Football
+time limit per test2 seconds
+memory limit per test256 megabytes
+inputstandard input
+outputstandard output
+Petya loves football very much. One day, as he was watching a football match, he was writing the players' current positions on a piece of paper. To simplify the situation he depicted it as a string consisting of zeroes and ones. A zero corresponds to players of one team; a one corresponds to players of another team. If there are at least 7 players of some team standing one after another, then the situation is considered dangerous. For example, the situation 00100110111111101 is dangerous and 11110111011101 is not. You are given the current situation. Determine whether it is dangerous or not.
+
+Input
+The first input line contains a non-empty string consisting of characters "0" and "1", which represents players. The length of the string does not exceed 100 characters. There's at least one player from each team present on the field.
+
+Output
+Print "YES" if the situation is dangerous. Otherwise, print "NO".
+
+Examples
+inputCopy
+001001
+outputCopy
+NO
+inputCopy
+1000000001
+outputCopy
+YES
+
+*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    string s;
+    cin>>s;
+    int size = s.size(),count=1,c1=1;
+    for(int i=0;i<size-1;i++){
+        if(s[i]==s[i+1]){
+            count++;
+        }
+        if(s[i] != s[i+1] || i==size-2){
+            if(count>=c1){
+                c1 = count;
+            }
+            count=1;
+        }
+    }
+    if(c1>=7){
+        cout<<"YES"<<endl;
+    }
+    else{
+        cout<<"NO"<<endl;
+    }
+
+    return 0;
+}
